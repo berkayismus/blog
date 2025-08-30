6ab9fe649fec9b6123afb2adb53dd459bdb731f0
berkayismus <berkayismus@users.noreply.github.com>
2025-08-30 14:36:00 +0000

    deploy: e4f90d1548aa8a75af7ac59e8379350e86fd96db

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "511132877"
+    serviceWorkerVersion: "3999232052"
   }
 });
